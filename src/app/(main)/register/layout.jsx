dc2c33db03e6efb560a5606740b0863e12d78940
thenoname09 <rafi.adnan365@gmail.com>
2026-09-23 16:01:37 +0600

    register metadata fix

---
+++ src/app/(main)/register/layout.jsx
@@ -0,0 +1,10 @@
+
+
+export const metadata = {
+  title: "Register - Fable",
+  description: "Create a new Fable account.",
+};
+
+export default function RegisterLayout({ children }) {
+  return <>{children}</>;
+}
